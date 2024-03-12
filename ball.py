644cfd35f285154839340ba1cb8cc63d343483b8
Neil <neilwonil@gmail.com>
2024-03-12 13:24:03 +1030

    Make the ball move.

--- ball.py
+++ ball.py
@@ -8,3 +8,8 @@ class Ball(Turtle):
         self.shape("circle")
         self.penup()
         self.goto(0, 0)
+
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
